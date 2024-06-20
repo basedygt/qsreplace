2361b93481bad6498eda525006f5584c3623f8af
basedygt <167074291+basedygt@users.noreply.github.com>
2024-06-20 14:42:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+pwd = path.abspath(path.dirname(__file__))
+with io.open(path.join(pwd, 'README.md'), encoding='utf-8') as readme:
+    desc = readme.read()
+
 setup(
     name='qsreplace',
     version='0.0.1',
     description='A utility to replace query parameters in URLs',
+    long_description=desc,
+    long_description_content_type='text/markdown',
     author='basedygt',
+    license='Apache-2.0 License',
     author_email='basedygt@gmail.com',
     url='https://github.com/basedygt/qsreplace',
     packages=find_packages(),
